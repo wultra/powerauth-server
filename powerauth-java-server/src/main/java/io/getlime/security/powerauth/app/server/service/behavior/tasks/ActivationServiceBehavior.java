bbf5e7415fc8c554a3b279a236a849e1b58a2271
Petr Dvorak <petr@inmite.eu>
2017-11-04 17:39:03 +0100

    Update crypto library to 0.17.0 version

--- powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/service/behavior/tasks/ActivationServiceBehavior.java
+++ powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/service/behavior/tasks/ActivationServiceBehavior.java
@@ -279,7 +279,7 @@ public class ActivationServiceBehavior {
                     );
 
                     // Assign the activation fingerprint
-                    activationFingerPrint = String.valueOf(powerAuthServerActivation.computeDevicePublicKeyFingerprint(devicePublicKey));
+                    activationFingerPrint = powerAuthServerActivation.computeDevicePublicKeyFingerprint(devicePublicKey);
 
                 }
 
