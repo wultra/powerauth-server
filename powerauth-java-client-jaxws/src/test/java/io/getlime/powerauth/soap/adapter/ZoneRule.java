540b922f014f51a09ec311dbd21b467223764551
Kaiser Ladislav <Ladislav.Kaiser7931@homecredit.eu>
2018-11-01 09:36:01 +0100

    rights

--- powerauth-java-client-jaxws/src/test/java/io/getlime/powerauth/soap/adapter/ZoneRule.java
+++ powerauth-java-client-jaxws/src/test/java/io/getlime/powerauth/soap/adapter/ZoneRule.java
@@ -1,3 +1,20 @@
+/*
+ * PowerAuth Server and related software components
+ * Copyright (C) 2017 Lime - HighTech Solutions s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.getlime.powerauth.soap.adapter;
 
 import java.time.ZoneId;
@@ -11,7 +28,7 @@ public class ZoneRule extends TestWatcher {
     private TimeZone systemDefault;
     private final ZoneId zone;
 
-    public ZoneRule(ZoneId zone) {
+    ZoneRule(ZoneId zone) {
         this.zone = zone;
     }
 
