cc88771e527da3f2d09311d28811d87715614e52
Lukáš Lukovský <lukas.lukovsky@gmail.com>
2019-05-14 08:30:25 +0200

    Add copyright and author

--- powerauth-java-server/src/test/java/io/getlime/security/powerauth/app/server/GetApplicationDetailTest.java
+++ powerauth-java-server/src/test/java/io/getlime/security/powerauth/app/server/GetApplicationDetailTest.java
@@ -1,3 +1,20 @@
+/*
+ * PowerAuth Server and related software components
+ * Copyright (C) 2019 Wultra s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package io.getlime.security.powerauth.app.server;
 
 import io.getlime.security.powerauth.app.server.database.model.entity.ApplicationEntity;
@@ -16,6 +33,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Tests for getting application detail.
+ *
+ * @author Lukas Lukovsky, lukas.lukovsky@gmail.com
+ */
 @SpringBootTest
 @RunWith(SpringJUnit4ClassRunner.class)
 public class GetApplicationDetailTest {
