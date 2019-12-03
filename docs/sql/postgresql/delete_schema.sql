ebe1f0e473f569d9da400d027738453405f08418
Roman Strobl <roman.strobl@wultra.com>
2019-12-03 16:31:08 +0100

    Update drop scripts

--- docs/sql/postgresql/delete_schema.sql
+++ docs/sql/postgresql/delete_schema.sql
@@ -10,6 +10,9 @@ DROP TABLE IF EXISTS "pa_master_keypair" CASCADE;
 DROP TABLE IF EXISTS "pa_signature_audit" CASCADE;
 DROP TABLE IF EXISTS "pa_integration" CASCADE;
 DROP TABLE IF EXISTS "pa_application_callback" CASCADE;
+DROP TABLE IF EXISTS "pa_recovery_config" CASCADE;
+DROP TABLE IF EXISTS "pa_recovery_puk" CASCADE;
+DROP TABLE IF EXISTS "pa_recovery_code" CASCADE;
 
 --
 --  Drop all sequences.
@@ -19,3 +22,6 @@ DROP SEQUENCE IF EXISTS "pa_application_version_seq";
 DROP SEQUENCE IF EXISTS "pa_master_keypair_seq";
 DROP SEQUENCE IF EXISTS "pa_signature_audit_seq";
 DROP SEQUENCE IF EXISTS "pa_activation_history_seq";
+DROP SEQUENCE IF EXISTS "pa_recovery_code_seq";
+DROP SEQUENCE IF EXISTS "pa_recovery_puk_seq";
+DROP SEQUENCE IF EXISTS "pa_recovery_config_seq";
