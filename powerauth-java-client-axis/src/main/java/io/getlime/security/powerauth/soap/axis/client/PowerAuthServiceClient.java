31a4a0f304db0dd37e132de66549214e4f1c5e82
Roman Strobl <roman.strobl@wultra.com>
2020-05-21 19:15:58 +0200

    Fix #427: Add activationOtp parameter in commitActivation client methods

--- powerauth-java-client-axis/src/main/java/io/getlime/security/powerauth/soap/axis/client/PowerAuthServiceClient.java
+++ powerauth-java-client-axis/src/main/java/io/getlime/security/powerauth/soap/axis/client/PowerAuthServiceClient.java
@@ -362,6 +362,22 @@ public class PowerAuthServiceClient {
     }
 
     /**
+     * Call the prepareActivation method of the PowerAuth 3.0 Server SOAP interface.
+     * @param activationId Activation ID for activation to be committed.
+     * @param externalUserId User ID of user who committed the activation. Use null value if activation owner caused the change.
+     * @param activationOtp Value of activation OTP. Specify the value only when activation OTP should be validated during activation commit.
+     * @return {@link io.getlime.powerauth.soap.v3.PowerAuthPortV3ServiceStub.CommitActivationResponse}
+     * @throws RemoteException In case of a business logic error.
+     */
+    public PowerAuthPortV3ServiceStub.CommitActivationResponse commitActivation(String activationId, String externalUserId, String activationOtp) throws RemoteException {
+        PowerAuthPortV3ServiceStub.CommitActivationRequest request = new PowerAuthPortV3ServiceStub.CommitActivationRequest();
+        request.setActivationId(activationId);
+        request.setExternalUserId(externalUserId);
+        request.setActivationOtp(activationOtp);
+        return this.commitActivation(request);
+    }
+
+    /**
      * Call the updateActivationOtp method of PowerAuth 3.1 Server SOAP interface.
      * @param activationId      Activation ID for activation to be updated.
      * @param externalUserId    User ID of user who updated the activation. Use null value if activation owner caused the change,
