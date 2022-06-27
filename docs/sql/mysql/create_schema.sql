a70e114391bf7a1710c36f47137030787bac757c
Roman Strobl <roman.strobl@wultra.com>
2022-06-27 16:55:45 +0200

    Fix #745: Add auditing DB tables into DDL

--- docs/sql/mysql/create_schema.sql
+++ docs/sql/mysql/create_schema.sql
@@ -266,6 +266,35 @@ CREATE TABLE shedlock (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 --
+-- Create audit log table.
+--
+CREATE TABLE audit_log (
+    audit_log_id       VARCHAR(36) PRIMARY KEY,
+    application_name   VARCHAR(256) NOT NULL,
+    audit_level        VARCHAR(32) NOT NULL,
+    audit_type         VARCHAR(256),
+    timestamp_created  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    message            TEXT NOT NULL,
+    exception_message  TEXT,
+    stack_trace        TEXT,
+    param              TEXT,
+    calling_class      VARCHAR(256) NOT NULL,
+    thread_name        VARCHAR(256) NOT NULL,
+    version            VARCHAR(256),
+    build_time         TIMESTAMP NULL
+) ENGINE=InnoDB AUTO_INCREMENT=1 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--
+-- Create audit parameters table.
+--
+CREATE TABLE audit_param (
+    audit_log_id       VARCHAR(36),
+    timestamp_created  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    param_key          VARCHAR(256),
+    param_value        VARCHAR(3072)
+) ENGINE=InnoDB AUTO_INCREMENT=1 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+--
 -- Indexes for better performance. InnoDB engine creates indexes on foreign keys automatically, so they are not included.
 --
 
@@ -298,3 +327,15 @@ CREATE INDEX `pa_operation_template_name_idx` ON `pa_operation_template` (`templ
 CREATE UNIQUE INDEX `pa_recovery_code_puk` ON `pa_recovery_puk`(`recovery_code_id`, `puk_index`);
 
 CREATE UNIQUE INDEX `pa_application_name` ON `pa_application`(`name`);
+
+--
+-- Auditing indexes.
+--
+CREATE INDEX audit_log_timestamp ON audit_log (timestamp_created);
+CREATE INDEX audit_log_application ON audit_log (application_name);
+CREATE INDEX audit_log_level ON audit_log (audit_level);
+CREATE INDEX audit_log_type ON audit_log (audit_type);
+CREATE INDEX audit_param_log ON audit_param (audit_log_id);
+CREATE INDEX audit_param_timestamp ON audit_param (timestamp_created);
+CREATE INDEX audit_param_key ON audit_param (param_key);
+CREATE FULLTEXT INDEX audit_param_value ON audit_param (param_value);
--- docs/sql/oracle/create_schema.sql
+++ docs/sql/oracle/create_schema.sql
@@ -258,6 +258,35 @@ CREATE TABLE shedlock (
 );
 
 --
+-- Create audit log table.
+--
+CREATE TABLE audit_log (
+    audit_log_id       VARCHAR2(36 CHAR) PRIMARY KEY,
+    application_name   VARCHAR2(256 CHAR) NOT NULL,
+    audit_level        VARCHAR2(32 CHAR) NOT NULL,
+    audit_type         VARCHAR2(256 CHAR),
+    timestamp_created  TIMESTAMP,
+    message            CLOB NOT NULL,
+    exception_message  CLOB,
+    stack_trace        CLOB,
+    param              CLOB,
+    calling_class      VARCHAR2(256 CHAR) NOT NULL,
+    thread_name        VARCHAR2(256 CHAR) NOT NULL,
+    version            VARCHAR2(256 CHAR),
+    build_time         TIMESTAMP
+);
+
+--
+-- Create audit parameters table.
+--
+CREATE TABLE audit_param (
+    audit_log_id       VARCHAR2(36 CHAR),
+    timestamp_created  TIMESTAMP,
+    param_key          VARCHAR2(256 CHAR),
+    param_value        VARCHAR2(4000 CHAR)
+);
+
+--
 --  Ref Constraints for Table PA_ACTIVATION
 --
 ALTER TABLE "PA_ACTIVATION" ADD CONSTRAINT "ACTIVATION_KEYPAIR_FK" FOREIGN KEY ("MASTER_KEYPAIR_ID") REFERENCES "PA_MASTER_KEYPAIR" ("ID") ENABLE;
@@ -370,3 +399,15 @@ CREATE INDEX PA_OPERATION_TS_CREATED_IDX ON PA_OPERATION(TIMESTAMP_CREATED);
 CREATE INDEX PA_OPERATION_TS_EXPIRES_IDX ON PA_OPERATION(TIMESTAMP_EXPIRES);
 
 CREATE INDEX PA_OPERATION_TEMPLATE_NAME_IDX ON PA_OPERATION_TEMPLATE(TEMPLATE_NAME);
+
+--
+-- Auditing indexes.
+--
+CREATE INDEX audit_log_timestamp ON audit_log (timestamp_created);
+CREATE INDEX audit_log_application ON audit_log (application_name);
+CREATE INDEX audit_log_level ON audit_log (audit_level);
+CREATE INDEX audit_log_type ON audit_log (audit_type);
+CREATE INDEX audit_param_log ON audit_param (audit_log_id);
+CREATE INDEX audit_param_timestamp ON audit_param (timestamp_created);
+CREATE INDEX audit_param_key ON audit_param (param_key);
+CREATE INDEX audit_param_value ON audit_param (param_value);
