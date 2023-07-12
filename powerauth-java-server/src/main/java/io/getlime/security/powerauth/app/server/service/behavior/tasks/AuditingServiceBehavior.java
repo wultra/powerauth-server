05c7ef5b5c69c48e9d1127de492feae6c124f6f9
Jan Dusil <jan.dusil@wultra.com>
2023-07-12 14:27:49 +0200

    Fix #925: Audit type missing in AuditingServiceBehavior - Add javadoc, remove redundant access modifier

--- powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/service/behavior/tasks/AuditingServiceBehavior.java
+++ powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/service/behavior/tasks/AuditingServiceBehavior.java
@@ -218,19 +218,44 @@ public class AuditingServiceBehavior {
         @lombok.NonNull private ActivationStatus activationStatus;
     }
 
-    protected enum AuditType {
+    /**
+     * This enum represents different types of audits.
+     */
+    enum AuditType {
+        /**
+         * Audit type for signatures.
+         */
         SIGNATURE("signature"),
 
+        /**
+         * Audit type for operations.
+         */
         OPERATION("operation"),
 
+        /**
+         * Audit type for activations.
+         */
         ACTIVATION("activation");
 
+        /**
+         * The code associated with each audit type.
+         */
         private final String code;
 
+        /**
+         * Constructor to initialize the audit type with its code.
+         *
+         * @param code the code of the audit type
+         */
         AuditType(final String code) {
             this.code = code;
         }
 
+        /**
+         * Get the code of the audit type.
+         *
+         * @return the code of the audit type
+         */
         public String getCode() {
             return this.code;
         }
