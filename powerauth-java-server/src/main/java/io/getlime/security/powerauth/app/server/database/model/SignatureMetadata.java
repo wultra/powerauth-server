4c19310f0c52b2de099a64e39ecededbece6a98f
Jan Dusil <jan.dusil@wultra.com>
2023-08-29 00:45:55 +0200

    Fix #971: Add column for signature metadata - Add NoArgsConstructor

--- powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/database/model/SignatureMetadata.java
+++ powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/database/model/SignatureMetadata.java
@@ -2,9 +2,11 @@ package io.getlime.security.powerauth.app.server.database.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignatureMetadata {
 
     private String signatureDataMethod;
