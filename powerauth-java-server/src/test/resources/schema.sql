8315528e9a995661496adc39a1ae9f63c8447426
Jan Pešek <jan.pesek@wultra.com>
2023-10-30 10:14:06 +0100

    Fix #1035: Remove ShedlockEntity (#1096)
    Co-authored-by: Lubos Racansky <lubos.racansky@wultra.com>

---
+++ powerauth-java-server/src/test/resources/schema.sql
@@ -0,0 +1,9 @@
+-- Scheduler lock table - https://github.com/lukas-krecan/ShedLock#configure-lockprovider
+CREATE TABLE IF NOT EXISTS shedlock
+(
+    name       VARCHAR(64)  NOT NULL,
+    lock_until TIMESTAMP    NOT NULL,
+    locked_at  TIMESTAMP    NOT NULL,
+    locked_by  VARCHAR(255) NOT NULL,
+    PRIMARY KEY (name)
+);
