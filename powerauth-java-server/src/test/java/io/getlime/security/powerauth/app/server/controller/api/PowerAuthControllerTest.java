8a3acf5faa09ac4bc01e59c7b5b024a35388255e
Jan Dusil <jan.dusil@wultra.com>
2024-01-03 03:17:39 +0100

    Fix #1099: Move tests from powerauth-tests to this repo - Move operation tests - Add javadoc - Edit sql for tests

--- powerauth-java-server/src/test/java/io/getlime/security/powerauth/app/server/controller/api/PowerAuthControllerTest.java
+++ powerauth-java-server/src/test/java/io/getlime/security/powerauth/app/server/controller/api/PowerAuthControllerTest.java
@@ -17,9 +17,15 @@
  */
 package io.getlime.security.powerauth.app.server.controller.api;
 
+import com.jayway.jsonpath.JsonPath;
 import com.wultra.core.audit.base.database.DatabaseAudit;
+import com.wultra.security.powerauth.client.model.request.OperationCreateRequest;
+import com.wultra.security.powerauth.client.model.response.OperationDetailResponse;
 import io.getlime.security.powerauth.app.server.database.model.entity.ActivationHistoryEntity;
 import io.getlime.security.powerauth.app.server.database.model.entity.ActivationRecordEntity;
+import io.getlime.security.powerauth.app.server.database.model.entity.OperationEntity;
+import io.getlime.security.powerauth.app.server.database.model.enumeration.OperationStatusDo;
+import io.getlime.security.powerauth.app.server.service.behavior.tasks.OperationServiceBehavior;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Tuple;
 import org.junit.jupiter.api.Test;
@@ -30,6 +36,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -62,6 +69,19 @@ class PowerAuthControllerTest {
     @Autowired
     private DatabaseAudit databaseAudit;
 
+    @Autowired
+    OperationServiceBehavior operationServiceBehavior;
+
+    /**
+     * Tests the update activation functionality.
+     * <p>
+     * This test performs a mock HTTP POST request to the activation name update endpoint.
+     * It verifies if the response status is OK and if the activation name in the response object
+     * matches the expected value. It also checks the database for the updated activation record
+     * and ensures the audit log is correctly updated.
+     *
+     * @throws Exception if the mockMvc.perform operation fails.
+     */
     @Test
     void testUpdateActivation() throws Exception {
         mockMvc.perform(post("/rest/v3/activation/name/update")
@@ -95,8 +115,7 @@ class PowerAuthControllerTest {
 
         databaseAudit.flush();
         final String expectedAuditMessage = "Updated activation with ID: e43a5dec-afea-4a10-a80b-b2183399f16b";
-        @SuppressWarnings("unchecked")
-        final List<Tuple> auditEntries = entityManager.createNativeQuery("select * from audit_log where message = :message", Tuple.class)
+        @SuppressWarnings("unchecked") final List<Tuple> auditEntries = entityManager.createNativeQuery("select * from audit_log where message = :message", Tuple.class)
                 .setParameter("message", expectedAuditMessage)
                 .getResultList();
         assertEquals(1, auditEntries.size());
@@ -107,6 +126,15 @@ class PowerAuthControllerTest {
         assertThat(param, containsString("\"reason\":\"ACTIVATION_NAME_UPDATED\""));
     }
 
+    /**
+     * Tests the update activation functionality with a bad request.
+     * <p>
+     * This test sends an incomplete request to the activation name update endpoint
+     * and expects a BadRequest (400) response. It checks if the error response
+     * contains the correct error message and error code.
+     *
+     * @throws Exception if the mockMvc.perform operation fails.
+     */
     @Test
     void testUpdateActivation_badRequest() throws Exception {
         final String expectedErrorMessage = "requestObject.activationId - must not be blank, requestObject.activationName - must not be blank, requestObject.externalUserId - must not be blank";
@@ -124,5 +152,89 @@ class PowerAuthControllerTest {
                 .andExpect(jsonPath("$.responseObject.code").value("ERR0024"))
                 .andExpect(jsonPath("$.responseObject.message").value(expectedErrorMessage))
                 .andExpect(jsonPath("$.responseObject.localizedMessage").value(expectedErrorMessage));
+    }
+
+    /**
+     * Tests the operation approval functionality.
+     * <p>
+     * This test first creates an operation and then performs a mock HTTP POST request
+     * to the operation approval endpoint. It verifies if the operation is approved successfully
+     * by checking the response status and response object. It also confirms that the
+     * operation entity in the database is updated accordingly.
+     *
+     * @throws Exception if the mockMvc.perform operation or operation creation fails.
+     */
+    @Test
+    void testOperationApprove() throws Exception {
+        final String applicationId = "PA_Tests";
+        final String userId = "test-user";
+        final String templateName = "test-template";
+        final String data = "A2";
+
+        final OperationCreateRequest operationCreateRequest = new OperationCreateRequest();
+        operationCreateRequest.setApplications(List.of(applicationId));
+        operationCreateRequest.setTemplateName(templateName);
+        operationCreateRequest.setUserId(userId);
+        final String operationId = operationServiceBehavior.createOperation(operationCreateRequest).getId();
+
+        mockMvc.perform(post("/rest/v3/operation/approve")
+                        .content("""
+                                {
+                                  "requestObject": {
+                                    "operationId": "%s",
+                                    "userId": "%s",
+                                    "applicationId": "%s",
+                                    "data": "%s",
+                                    "signatureType": "POSSESSION_KNOWLEDGE"
+                                  }
+                                }
+                                """.formatted(operationId, userId, applicationId, data))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.responseObject.operation.status").value("APPROVED"))
+                .andExpect(jsonPath("$.responseObject.operation.data").value(data))
+                .andExpect(jsonPath("$.responseObject.operation.templateName").value(templateName))
+                .andExpect(jsonPath("$.responseObject.operation.id").value(operationId));
+
+        final OperationEntity operation = entityManager.find(OperationEntity.class, operationId);
+        assertEquals(templateName, operation.getTemplateName());
+        assertEquals(data, operation.getData());
+        assertEquals(OperationStatusDo.APPROVED, operation.getStatus());
+    }
+
+    /**
+     * Tests the operation creation functionality.
+     * <p>
+     * This test performs a mock HTTP POST request to the operation creation endpoint.
+     * It verifies if the response status is OK and if the operation is created with
+     * a status of "PENDING". The test also confirms that the operation entity is
+     * correctly created in the database with the expected template name.
+     *
+     * @throws Exception if the mockMvc.perform operation fails.
+     */
+    @Test
+    void testOperationCreate() throws Exception {
+        MvcResult result = mockMvc.perform(post("/rest/v3/operation/create")
+                        .content("""
+                                {
+                                  "requestObject": {
+                                    "applications": ["PA_Tests"],
+                                    "templateName": "test-template"
+                                  }
+                                }
+                                """)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.responseObject.status").value("PENDING")).andReturn();
+
+
+        final String operationId = JsonPath.read(result.getResponse().getContentAsString(), "$.responseObject.id");
+
+        final OperationEntity operation = entityManager.find(OperationEntity.class, operationId);
+        assertEquals("test-template", operation.getTemplateName());
     }
 }
