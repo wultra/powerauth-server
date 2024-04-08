a8ba2b5415d84d5a48dfb8c0e2a35084da82fb6d
Lubos Racansky <lubos.racansky@gmail.com>
2024-04-08 08:11:21 +0200

    Add AuthenticatorDataDeserializerTest#testDeserialize_credentials

--- powerauth-fido2/src/test/java/com/wultra/powerauth/fido2/rest/model/converter/serialization/AuthenticatorDataDeserializerTest.java
+++ powerauth-fido2/src/test/java/com/wultra/powerauth/fido2/rest/model/converter/serialization/AuthenticatorDataDeserializerTest.java
@@ -19,6 +19,10 @@ package com.wultra.powerauth.fido2.rest.model.converter.serialization;
 
 import com.wultra.powerauth.fido2.rest.model.entity.AttestedCredentialData;
 import com.wultra.powerauth.fido2.rest.model.entity.AuthenticatorData;
+import com.wultra.powerauth.fido2.rest.model.entity.PublicKeyObject;
+import com.wultra.powerauth.fido2.rest.model.enumeration.CurveType;
+import com.wultra.powerauth.fido2.rest.model.enumeration.ECKeyType;
+import com.wultra.powerauth.fido2.rest.model.enumeration.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -48,6 +52,33 @@ class AuthenticatorDataDeserializerTest {
         assertNull(attestedCredentialData.getCredentialId());
         assertNull(attestedCredentialData.getAaguid());
         assertNull(attestedCredentialData.getPublicKeyObject());
+    }
+
+    @Test
+    void testDeserialize_credentials() throws Exception{
+        final AuthenticatorData result = AuthenticatorDataDeserializer.deserialize("SZYN5YgOjGh0NBcPZHZgW4/krrmihjLHmVzzuoMdl2NdAAAAALraVWanqkAfvZZFYZpVEg0AENntBM0k3kUGHZJgXctZB2mlAQIDJiABIVggUnRRgaXiOSdKFnu6u04mUQNMDyuHWDODUdIcrt4Ca9wiWCBEq2quWbx976dPV7Ajt8yR5+4h1tnfT8X5ey7PT0utKA==");
+
+        assertTrue(result.getFlags().isUserPresent());
+        assertFalse(result.getFlags().isReservedBit2());
+        assertTrue(result.getFlags().isUserVerified());
+        assertTrue(result.getFlags().isBackupEligible());
+        assertTrue(result.getFlags().isBackupState());
+        assertFalse(result.getFlags().isReservedBit6());
+        assertTrue(result.getFlags().isAttestedCredentialsIncluded());
+        assertFalse(result.getFlags().isExtensionDataIncluded());
+        assertEquals(0, result.getSignCount());
+
+        final AttestedCredentialData attestedCredentialData = result.getAttestedCredentialData();
+        assertNotNull(attestedCredentialData.getCredentialId());
+        assertNotNull(attestedCredentialData.getAaguid());
+
+        final PublicKeyObject publicKeyObject = attestedCredentialData.getPublicKeyObject();
+        assertNotNull(publicKeyObject);
+        assertEquals(SignatureAlgorithm.ES256, publicKeyObject.getAlgorithm());
+        assertEquals(CurveType.P256, publicKeyObject.getCurveType());
+        assertEquals(ECKeyType.UNCOMPRESSED, publicKeyObject.getKeyType());
+        assertNotNull(publicKeyObject.getPoint().getX());
+        assertNotNull(publicKeyObject.getPoint().getY());
     }
 
 }
