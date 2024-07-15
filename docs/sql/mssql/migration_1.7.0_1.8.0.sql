9142d1d87d243a7862df3d43ccb9ac4942d0c545
Jan Pešek <jan.pesek@wultra.com>
2024-07-15 13:55:50 +0100

    Fix #341: Optimization: Callbacks invocation

--- docs/sql/mssql/migration_1.7.0_1.8.0.sql
+++ docs/sql/mssql/migration_1.7.0_1.8.0.sql
@@ -17,3 +17,24 @@ GO
 -- Add status_reason column to pa_operation table.
 ALTER TABLE pa_operation ADD status_reason varchar(32);
 GO
+
+-- Changeset powerauth-java-server/1.8.x/20240704-callback-event-table.xml::1::Jan Pesek
+-- Create a new table pa_callback_event
+CREATE TABLE pa_application_callback_event (id varchar(36) NOT NULL, application_callback_id varchar(37) NOT NULL, callback_data varchar (max) NOT NULL, status varchar(255) NOT NULL, timestamp_created datetime2(6) CONSTRAINT DF_pa_application_callback_event_timestamp_created DEFAULT GETDATE(), timestamp_last_call datetime2(6), timestamp_next_call datetime2(6), timestamp_delete_after datetime2(6), attempts int CONSTRAINT DF_pa_application_callback_event_attempts DEFAULT 0, CONSTRAINT PK_PA_APPLICATION_CALLBACK_EVENT PRIMARY KEY (id), CONSTRAINT pa_application_callback_id_fk FOREIGN KEY (application_callback_id) REFERENCES pa_application_callback(id));
+GO
+
+-- Changeset powerauth-java-server/1.8.x/20240704-callback-event-table.xml::2::Jan Pesek
+-- Add max_attempts column to pa_application_callback table.
+ALTER TABLE pa_application_callback ADD max_attempts int;
+GO
+
+-- Changeset powerauth-java-server/1.8.x/20240704-callback-event-table.xml::3::Jan Pesek
+-- Add initial_backoff column to pa_application_callback table.
+ALTER TABLE pa_application_callback ADD initial_backoff int;
+GO
+
+-- Changeset powerauth-java-server/1.8.x/20240704-callback-event-table.xml::4::Jan Pesek
+-- Add retention_period column to pa_application_callback table.
+ALTER TABLE pa_application_callback ADD retention_period varchar(255);
+GO
+
