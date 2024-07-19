cde106c775484cf38298381c748af57ba246e81b
Jan Pešek <jan.pesek@wultra.com>
2024-07-19 09:41:37 +0100

    Use final

--- powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/service/callbacks/CallbackUrlEventService.java
+++ powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/service/callbacks/CallbackUrlEventService.java
@@ -253,17 +253,16 @@ public class CallbackUrlEventService {
      */
     private RestClient getRestClient(final CallbackUrlEntity callbackUrlEntity) throws RestClientException, GenericServiceException {
         final String cacheKey = getRestClientCacheKey(callbackUrlEntity);
-        RestClient restClient;
         synchronized (restClientCacheLock) {
-            restClient = restClientCache.get(cacheKey);
+            final RestClient restClient = restClientCache.get(cacheKey);
             if (restClient == null) {
                 logger.debug("REST client not found in cache, initializing new REST client, callback cache key: {}", cacheKey);
-                restClient = createRestClientAndStoreInCache(callbackUrlEntity);
+                return createRestClientAndStoreInCache(callbackUrlEntity);
             } else {
                 logger.debug("REST client found in cache, callback cache key: {}", cacheKey);
+                return restClient;
             }
         }
-        return restClient;
     }
 
     /**
