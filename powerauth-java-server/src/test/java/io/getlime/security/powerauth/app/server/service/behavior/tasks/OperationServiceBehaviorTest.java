8d09fc1ed033ccbe24797b03aafb71ceeda169c3
Lubos Racansky <lubos.racansky@gmail.com>
2024-07-24 07:28:05 +0200

    Fix OperationServiceBehaviorTest
    A follow-up #1606

--- powerauth-java-server/src/test/java/io/getlime/security/powerauth/app/server/service/behavior/tasks/OperationServiceBehaviorTest.java
+++ powerauth-java-server/src/test/java/io/getlime/security/powerauth/app/server/service/behavior/tasks/OperationServiceBehaviorTest.java
@@ -208,10 +208,10 @@ class OperationServiceBehaviorTest {
                 new-line""");
 
         final OperationDetailResponse operationDetailResponse = operationService.createOperation(request);
-        assertTrue(operationRepository.findOperation(operationDetailResponse.getId()).isPresent());
-        final OperationEntity savedEntity = operationRepository.findOperation(operationDetailResponse.getId()).get();
+        final Optional<OperationEntity> savedEntity = operationRepository.findOperationWithLock(operationDetailResponse.getId());
+        assertTrue(savedEntity.isPresent());
 
-        assertEquals("A0*T\\\\foo\\*hoo\\nnew-line", savedEntity.getData());
+        assertEquals("A0*T\\\\foo\\*hoo\\nnew-line", savedEntity.get().getData());
     }
 
     /**
