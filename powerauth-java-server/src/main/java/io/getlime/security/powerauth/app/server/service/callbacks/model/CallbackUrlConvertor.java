88d153c563aa79a35b9eeb0c6dfd1b71ff03fd31
Jan Pešek <jan.pesek@wultra.com>
2024-08-02 10:55:38 +0100

    Add service data classes

---
+++ powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/service/callbacks/model/CallbackUrlConvertor.java
@@ -0,0 +1,49 @@
+/*
+ * PowerAuth Server and related software components
+ * Copyright (C) 2024 Wultra s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.getlime.security.powerauth.app.server.service.callbacks.model;
+
+import io.getlime.security.powerauth.app.server.database.model.entity.CallbackUrlEntity;
+import io.getlime.security.powerauth.app.server.database.model.entity.CallbackUrlEventEntity;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+/**
+ * Convertor between data classes related to Callbacks URL.
+ * @author Jan Pesek, jan.pesek@wultra.com
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CallbackUrlConvertor {
+
+    public static CallbackUrlEvent convert(final CallbackUrlEventEntity callbackUrlEventEntity) {
+        return CallbackUrlEvent.builder()
+                .callbackUrlEventEntityId(callbackUrlEventEntity.getId())
+                .callbackData(callbackUrlEventEntity.getCallbackData())
+                .callbackUrlEventConfiguration(convert(callbackUrlEventEntity.getCallbackUrlEntity()))
+                .build();
+    }
+
+    public static CallbackUrlEventConfiguration convert(final CallbackUrlEntity callbackUrlEntity) {
+        return CallbackUrlEventConfiguration.builder()
+                .callbackUrlEntityId(callbackUrlEntity.getId())
+                .callbackUrl(callbackUrlEntity.getCallbackUrl())
+                .authentication(callbackUrlEntity.getAuthentication())
+                .build();
+    }
+
+}
