54df27b23c7600402e929707de7cd1bdef16b33f
Roman Strobl <roman.strobl@wultra.com>
2025-03-13 09:00:47 +0100

    Check application RID

--- powerauth-java-server/src/main/java/com/wultra/security/powerauth/app/server/service/crypto/v3/EncryptionServiceEc256.java
+++ powerauth-java-server/src/main/java/com/wultra/security/powerauth/app/server/service/crypto/v3/EncryptionServiceEc256.java
@@ -144,6 +144,12 @@ public class EncryptionServiceEc256 implements EncryptionService {
     }
 
     private DecryptionResult decryptInActivationScope(EciesEncryptedRequest eciesRequest, String protocolVersion, ApplicationVersionEntity applicationVersion, ActivationRecordEntity activation, EncryptorId encryptorId) throws GenericServiceException, InvalidKeySpecException, CryptoProviderException, EncryptorException, GenericCryptoException, InvalidKeyException {
+        // Check that application key from request belongs to same application as activation ID from request
+        if (!applicationVersion.getApplication().getRid().equals(activation.getApplication().getRid())) {
+            logger.warn("Application version does not match, application key: {}", applicationVersion.getApplicationKey());
+            // Rollback is not required, database is not used for writing
+            throw localizationProvider.buildExceptionForCode(ServiceError.INVALID_APPLICATION);
+        }
         final PrivateKey privateKey;
         final String serverPrivateKeyFromEntity = activation.getServerPrivateKeyBase64();
         final EncryptionMode serverPrivateKeyEncryptionMode = activation.getServerPrivateKeyEncryption();
