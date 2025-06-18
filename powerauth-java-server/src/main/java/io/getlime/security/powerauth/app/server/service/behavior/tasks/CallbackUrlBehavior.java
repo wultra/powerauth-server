d4fc9449b7f511baa70c0a0f0887dc4d792af507
Luboš Račanský <lubos.racansky@wultra.com>
2025-06-18 08:46:43 +0200

    Fix formatting of additionalData in callbacks (#1976)
    * Fix formatting of additionalData in callbacks
    
    A follow-up to #1962
    
    (cherry picked from commit 73cf02e0334bdfc86609d140fbaa2eb7ea3df814)

--- powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/service/behavior/tasks/CallbackUrlBehavior.java
+++ powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/service/behavior/tasks/CallbackUrlBehavior.java
@@ -17,6 +17,8 @@
  */
 package io.getlime.security.powerauth.app.server.service.behavior.tasks;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.wultra.core.rest.client.base.RestClientException;
 import com.wultra.security.powerauth.client.model.entity.CallbackUrl;
@@ -77,6 +79,7 @@ public class CallbackUrlBehavior {
     private final CallbackUrlAuthenticationEncryptor callbackUrlAuthenticationEncryptor;
     private final LoadingCache<String, CachedRestClient> restClientCache;
     private final PowerAuthCallbacksConfiguration powerAuthCallbacksConfiguration;
+    private final ObjectMapper objectMapper;
 
     /**
      * Creates a new callback URL record for application with given ID.
@@ -339,7 +342,7 @@ public class CallbackUrlBehavior {
      * @param activation Activation entity.
      * @return Callback data to send.
      */
-    private Map<String, Object> prepareCallbackDataActivation(CallbackUrlEntity callbackUrlEntity, ActivationRecordEntity activation) {
+    protected Map<String, Object> prepareCallbackDataActivation(CallbackUrlEntity callbackUrlEntity, ActivationRecordEntity activation) {
         final Map<String, Object> callbackData = new HashMap<>();
         callbackData.put("type", "ACTIVATION");
         callbackData.put("activationId", activation.getActivationId());
@@ -370,8 +373,13 @@ public class CallbackUrlBehavior {
         if (callbackUrlEntity.getAttributes().contains("applicationId")) {
             callbackData.put("applicationId", activation.getApplication().getId());
         }
-        if (callbackUrlEntity.getAttributes().contains("additionalData")) {
-            callbackData.put("additionalData", activation.getAdditionalData());
+        if (callbackUrlEntity.getAttributes().contains("additionalData") && activation.getAdditionalData() != null) {
+            try {
+                final Object additionalData = objectMapper.readValue(activation.getAdditionalData(), Object.class);
+                callbackData.put("additionalData", additionalData);
+            } catch (JsonProcessingException e) {
+                logger.error("Unable to deserialize additionalData: {}, activationId: {}", activation.getAdditionalData(), activation.getActivationId(), e);
+            }
         }
         return callbackData;
     }
