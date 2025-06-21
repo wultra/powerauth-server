d076685ec6f3c849fb8566987ef61b39af3b40bf
Roman Strobl <roman.strobl@wultra.com>
2025-06-21 10:47:51 +0200

    Exclude nonce from ToString
    (cherry picked from commit 30dae9c2c35c65350f3976c27453d8b5c86e8541)

--- powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/service/model/UniqueValueParam.java
+++ powerauth-java-server/src/main/java/io/getlime/security/powerauth/app/server/service/model/UniqueValueParam.java
@@ -21,6 +21,7 @@ package io.getlime.security.powerauth.app.server.service.model;
 
 import io.getlime.security.powerauth.app.server.database.model.enumeration.UniqueValueType;
 import lombok.Data;
+import lombok.ToString;
 
 /**
  * Parameters for unique value data checks in replay attack protection.
@@ -31,6 +32,7 @@ import lombok.Data;
 public class UniqueValueParam {
 
     private String ephemeralPublicKey;
+    @ToString.Exclude
     private String nonce;
     private String identifier;
     private UniqueValueType uniqueValueType;
