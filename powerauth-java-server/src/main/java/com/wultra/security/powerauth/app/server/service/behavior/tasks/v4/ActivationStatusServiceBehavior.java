560ecbfab10118b3d940ec2358b2681a15e82783
Jan Pešek <jan.pesek@wultra.com>
2025-11-05 13:47:57 +0100

    Move major version constant to a global settings

--- powerauth-java-server/src/main/java/com/wultra/security/powerauth/app/server/service/behavior/tasks/v4/ActivationStatusServiceBehavior.java
+++ powerauth-java-server/src/main/java/com/wultra/security/powerauth/app/server/service/behavior/tasks/v4/ActivationStatusServiceBehavior.java
@@ -70,11 +70,6 @@ import java.util.Optional;
 @AllArgsConstructor
 public class ActivationStatusServiceBehavior {
 
-    /**
-     * Current PowerAuth protocol major version. Activations created with lower version will be upgraded to this version.
-     */
-    private static final byte POWERAUTH_PROTOCOL_VERSION = 0x4;
-
     private static final byte STATUS_FLAG_ACTIVATION_CONFIRMATION = 1;
     private static final byte STATUS_FLAG_UPGRADE_CONFIRMATION    = 2;
     private static final byte STATUS_FLAG_UNSUPPORTED_ALGORITHM   = 4;
@@ -197,7 +192,7 @@ public class ActivationStatusServiceBehavior {
                         final ActivationStatusBlobInfo statusBlobInfo = new ActivationStatusBlobInfo();
                         statusBlobInfo.setActivationStatus(activation.getActivationStatus().getByte());
                         statusBlobInfo.setCurrentVersion(activation.getVersion().byteValue());
-                        statusBlobInfo.setUpgradeVersion(POWERAUTH_PROTOCOL_VERSION);
+                        statusBlobInfo.setUpgradeVersion(PowerAuthServiceConfiguration.POWERAUTH_PROTOCOL_VERSION);
                         statusBlobInfo.setFailedAttempts(activation.getFailedAttempts().byteValue());
                         statusBlobInfo.setMaxFailedAttempts(activation.getMaxFailedAttempts().byteValue());
                         statusBlobInfo.setCtrLookAhead((byte) powerAuthServiceConfiguration.getAuthenticationCodeValidationLookahead());
