580420bd454061fea839ce052aa4440ea25a9fb6
Roman Strobl <roman.strobl@wultra.com>
2025-11-10 19:15:42 +0100

    Improve service logic

--- powerauth-java-server/src/main/java/com/wultra/security/powerauth/app/server/service/behavior/tasks/ActivationInitServiceBehavior.java
+++ powerauth-java-server/src/main/java/com/wultra/security/powerauth/app/server/service/behavior/tasks/ActivationInitServiceBehavior.java
@@ -180,15 +180,16 @@ public class ActivationInitServiceBehavior {
                                     activationCode.getBytes(StandardCharsets.UTF_8),
                                     application
                             ) : null;
-            final CryptographyService cryptographyServiceP384MlL3 = cryptographyServiceFactory.getService(SharedSecretAlgorithm.EC_P384_ML_L3);
+            final CryptographyService cryptographyServiceP384 = cryptographyServiceFactory.getService(SharedSecretAlgorithm.EC_P384);
             final byte[] activationSignatureV4Ecdsa = (supportedAlgorithms.contains(SharedSecretAlgorithm.EC_P384)
                     || supportedAlgorithms.contains(SharedSecretAlgorithm.EC_P384_ML_L3))
                     || supportedAlgorithms.contains(SharedSecretAlgorithm.EC_P384_ML_L5)
-                    ? cryptographyServiceP384MlL3.generateSignatureForApplication(
+                    ? cryptographyServiceP384.generateSignatureForApplication(
                             KeyType.ECDSA_P384,
                             activationCode.getBytes(StandardCharsets.UTF_8),
                             application
                     ) : null;
+            final CryptographyService cryptographyServiceP384MlL3 = cryptographyServiceFactory.getService(SharedSecretAlgorithm.EC_P384_ML_L3);
             final byte[] activationSignatureV4Mldsa65 = supportedAlgorithms.contains(SharedSecretAlgorithm.EC_P384_ML_L3) ?
                     cryptographyServiceP384MlL3.generateSignatureForApplication(
                             KeyType.MLDSA_65,
