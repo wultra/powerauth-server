27eec75b43c33c195f20045820fbd2dcaef87aaf
filipbursik-wultra <filip.bursik@wultra.com>
2025-11-25 11:49:47 +0200

    Fix #2152: Add Docker and CI for PowerAuth Java Server (#2153)

---
+++ powerauth-java-server/docker-init/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+KEEP_RUNNING=${KEEP_RUNNING:-false}
+KEEP_RUNNING_PORT=${KEEP_RUNNING_PORT:-666}
+
+liquibase --headless=true --log-level=INFO \
+    --changeLogFile="changelog/powerauth-java-server/db.changelog-module.xml" \
+    --username="${POWERAUTH_SERVER_DATASOURCE_USERNAME:-}" \
+    --password="${POWERAUTH_SERVER_DATASOURCE_PASSWORD:-}" \
+    --url="${POWERAUTH_SERVER_DATASOURCE_URL}" \
+    update
+
+if [[ -n "${KEEP_RUNNING}" && "${KEEP_RUNNING}" == "true" ]]; then
+    echo "Container kept alive via KEEP_RUNNING and KEEP_RUNNING_PORT flag"
+    RESPONSE="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 15\r\n\r\nHello from nc!\n"
+
+    while true; do
+      echo -e "$RESPONSE" | nc -l -p "$KEEP_RUNNING_PORT"
+    done
+fi
